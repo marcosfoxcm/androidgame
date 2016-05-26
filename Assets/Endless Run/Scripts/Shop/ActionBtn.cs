89c7d6b99288a2b6c63d5a1b717b11a20861ef35
marcosfoxcm <marcosfox.projetos@gmail.com>
2016-05-25 23:50:30 -0300

    inicio
    inicio

---
+++ Assets/Endless Run/Scripts/Shop/ActionBtn.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionBtn : MonoBehaviour {
+
+	public Texture2D normal;
+	public Texture2D actived;
+	public string levelName;
+	
+	public AudioClip sfxButton;
+
+	private bool ready;
+
+	void OnMouseDown(){
+		this.GetComponent<GUITexture>().texture = actived;
+		
+		if(sfxButton != null)
+			AudioSource.PlayClipAtPoint(sfxButton,transform.position);
+	}
+
+
+	void OnMouseUpAsButton(){
+		this.GetComponent<GUITexture>().texture = normal;
+		Application.LoadLevel (levelName);
+	}
+	
+
+	void OnMouseExit(){
+		this.GetComponent<GUITexture>().texture = normal;
+	}
+}
